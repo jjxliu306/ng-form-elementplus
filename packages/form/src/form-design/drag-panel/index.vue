99fc1b0cf03f8926d8ad6415209f846936ffeda8
lyf <jjxliu306@163.com>
2021-12-09 09:14:34 +0800

    同步到dragpanel

--- packages/form/src/form-design/drag-panel/index.vue
+++ packages/form/src/form-design/drag-panel/index.vue
@@ -1,11 +1,11 @@
-	<!--
+<!--
  * author lyf
  * date 2020-07-06
  * description 可拖拽元素的列表 包含基础组件和布局组件
  --> 
 <template>
 	<el-card header="组件列表" class="box-card form-item ">
-		<el-collapse v-model="actives"  >
+		<el-collapse v-model="actives" accordion >
 		  	<el-collapse-item title="基础组件" name="1">
 		  		<DragItem 
 	                :list="basicsArray"
@@ -13,8 +13,17 @@
 	                @handleListPush="handleListPush"
 	                @start="handleStart"/>
 		  	</el-collapse-item>
+		  		 <!-- 个性化控件 -->
+            <el-collapse-item v-if="personalArray.length > 0" title="个性化组件" name="2" >
+              <DragItem
+                :list="personalArray"
+                @generateKey="generateKey"
+                @handleListPush="handleListPush"
+                @start="handleStart"
+              />
+            </el-collapse-item>
 		  	 <!-- 自定义控件 -->
-            <el-collapse-item v-if="customComponents.length > 0" title="自定义组件" name="2" >
+            <el-collapse-item v-if="customComponents.length > 0" title="自定义组件" name="3" >
               <DragItem
                 :list="customComponents"
                 @generateKey="generateKey"
@@ -22,7 +31,7 @@
                 @start="handleStart"
               />
             </el-collapse-item>
-		  	<el-collapse-item title="布局组件" name="3">
+		  	<el-collapse-item title="布局组件" name="4">
 		  		<DragItem 
 	                :list="layoutArray"
 	                @generateKey="generateKey"
@@ -34,7 +43,7 @@
 	</el-card>
 </template>
 <script>
-import { basicsList,  layoutList , noModelList} from "../config.js";
+import { basicsList,  layoutList ,personalList, noModelList} from "../config.js";
 import DragItem from './item'
 export default {
 	components: {
@@ -42,7 +51,7 @@ export default {
 	},
 	data(){
 		return {
-			actives:['1','2','3'],
+			actives:['1'],
 			noModel: noModelList,
 		    startType: "",
 		    data: {
@@ -57,17 +66,31 @@ export default {
 		      },
 		      previewOptions: {
 		        width: 850
-		    },
+		    }, 
 		    selectItem: {
 		       key: ""
-		    } ,
-		    customComponents: []
+		    } 
 		}
 	},
 	computed: {
 	    basicsArray() {
 	      	// 计算需要显示的基础字段
 	      	const blist = basicsList
+	      	blist.forEach(t=>{
+	      		t.options.dynamicHide = false
+        		t.options.dynamicHideValue = ''
+        		if(!t.key) { 
+        		 	const key = t.type + "_" + new Date().getTime()
+        		 	t['key'] = key 
+        		 	t['model'] = key
+        		}
+
+	      	}) ;
+	      	return blist
+	    },
+	    personalArray(){
+	    	// 计算需要显示的基础字段
+	      	const blist = personalList
 	      	blist.forEach(t=>{
 	      		t.options.dynamicHide = false
         		t.options.dynamicHideValue = ''
@@ -95,37 +118,32 @@ export default {
 	    } 
 	},
 	created() { 
-		if(window.customComponents) {
-			this.customComponents = window.customComponents
-
-			// 2021-05-17 lyf 初始化回填默认key和model
-			if(this.customComponents && this.customComponents.length > 0) {
-				this.customComponents.forEach(t=>{
-					if(!t.key) { 
-	        		 	const key = t.type + "_" + new Date().getTime()
-	        		 	t['key'] = key 
-        		 		t['model'] = key
-	        		}
-				})
-			}
+		// 2021-05-17 lyf 初始化回填默认key和model
+		if(this.customComponents && this.customComponents.length > 0) {
+			this.customComponents.forEach(t=>{
+				if(!t.key) { 
+	        		const key = t.type + "_" + new Date().getTime()
+	        		t['key'] = key 
+        		 	t['model'] = key
+	        	}
+			})
 		}
-		 
+	},
+	inject: {
+	    customComponents: {
+	      from: 'customC',
+	      default: ()=>[]
+	    },
 	},
 	methods: {
 		generateKey(list, index) {
 	      // 生成key值 
-	      if(index === undefined) return 
 	      const key = list[index].type + "_" + new Date().getTime();
-	  	  list[index] = {
+	      this.$set(list, index, {
 	        ...list[index],
 	        key,
 	        model: key
-	      }
-	      // this.$set(list, index, {
-	      //   ...list[index],
-	      //   key,
-	      //   model: key
-	      // });
+	      });
 	      if (this.noModel.includes(list[index].type)) {
 	        // 删除不需要的model属性
 	        delete list[index].model;
@@ -159,10 +177,9 @@ export default {
 	     
 	    },
 	    handleStart(list,index) {
-	    	if(index === undefined) return
 	    	this.generateKey(list,index)
-	      	const type = list[index].type
-	      	this.startType = type;
+	      const type = list[index].type
+	      this.startType = type;
 
 	    },
 	}
