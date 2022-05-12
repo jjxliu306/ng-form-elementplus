9862dbde7a5ace25dd7d98e70ddaf913430f4ea0
林星辉 <lxinghui@linewell.com>
2022-05-12 11:14:15 +0800

    vue3.0cli创建的vue3.0项目main.js中引入就编译错误 把源码中install (app: App<Element>)  改成 install (app)编译正常,并且能打开表单页

--- packages/index.ts
+++ packages/index.ts
@@ -1,16 +1,16 @@
- 
-import NgFormDesign from './form/src/form-design' 
-import NgFormBuild from './form/src/form-build' 
+
+import NgFormDesign from './form/src/form-design'
+import NgFormBuild from './form/src/form-build'
 import { App } from 'vue'
 
-function install (app: App<Element>) {
- 
+function install (app) {
+
   app.component('ng-form-design', NgFormDesign)
   app.component('ng-form-build', NgFormBuild)
 }
 
 export {
-  NgFormBuild, 
+  NgFormBuild,
   NgFormDesign
 }
 
